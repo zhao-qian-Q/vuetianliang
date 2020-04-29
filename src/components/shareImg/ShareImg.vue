831fb34a5803fb39a0b5e1a2b83698c7f0bbce8b
hapi <1216850715@qq.com>
2020-04-29 23:44:26 +0800

    图片分享页

--- src/components/shareImg/ShareImg.vue
+++ src/components/shareImg/ShareImg.vue
@@ -1,34 +1,50 @@
 <template>
   <div>
-    <!-- 轮播图 -->
-    <loopBox :getloops="loopList"></loopBox>
+   
+    <div class="mui-card" v-for='item in shareImgList' :key='item.id'>
+				<div class="mui-card-header mui-card-media">
+					<img src="/../images/menu1.png" />
+					<div class="mui-media-body">
+						小M
+						<p>发表于{{item.passtime}}</p>
+					</div>
+				</div>
+				<div class="mui-card-content" >
+					<img :src="item.img_url" alt="" width="100%"/>
+				</div>
+				<div class="mui-card-footer">
+					<a class="mui-card-link">分享</a>
+					<a class="mui-card-link">点赞</a>
+					<a class="mui-card-link">下载</a>
+				</div>
+			</div>
   </div>
 </template>
 <script>
-import loopBox from "../loopcompontent/LoopCompontent.vue";
+// import loopBox from "../loopcompontent/LoopCompontent.vue";
 export default {
   data() {
     return {
-      loopList: []
+      shareImgList:[]
     };
   },
   created() {
-    this.getLoop();
+    this.getshareImg();
   },
   methods: {
-    getLoop() {
+    getshareImg() {
       this.$http
-        .get("http://yapi.shangyuninfo.com/mock/121/api/getLoop")
+        .get("http://yapi.shangyuninfo.com/mock/121/api/getnewslist")
         .then(result => {
-          // console.log(result);
-          this.loopList = result.body.message;
+          console.log(result.body.message);
+          this.shareImgList = result.body.message;
           // console.log(this.loopList);
         })
         .catch();
     }
   },
   components: {
-    loopBox
+    // loopBox
   }
 };
 </script>
